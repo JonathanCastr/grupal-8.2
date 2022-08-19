127746cd25324c4070ee8b7661a9be328005fa55
JonathanCastr <jcastro@itpro.cl>
2022-08-19 13:04:28 -0400

    subida trabajo grupal 8.2
    Volvimos a subir el trabajo por problemas técnicos

---
+++ app/controllers/portada_controller.rb
@@ -0,0 +1,16 @@
+class PortadaController < ApplicationController
+  def deportes
+  end
+
+  def nacional
+  end
+
+  def farandula
+  end
+
+  def internacional
+  end
+
+  def politica
+  end
+end
